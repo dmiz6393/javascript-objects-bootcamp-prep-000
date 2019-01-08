8fe5cd7be833f03db203fe84b2c32c6fa79b9e9b
Danielle Mizrachi <dmiz@brandeis.edu>
2019-01-08 12:21:57 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,11 @@ const playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-   playlist[artistName]= 'songTitle'
-  return updatePlaylist
+   playlist[artistName]= 'songTitle'; 
+  return updatePlaylist;
 }
 
+function removeFromPlaylist(playlist,artistName) {
+  delete playlist.artistName;
+  return playlist;
+}
